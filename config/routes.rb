fb9b34debeabb55f0ab7b9a0cbde4e00cfba1a79
matk0 <matej.lukasik@gmail.com>
2016-09-25 09:54:52 +0200

    Implement creating tickets for a project.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
+  get 'tickets/index'
+
+  get 'tickets/new'
+
+  get 'tickets/create'
+
+  get 'tickets/update'
+
+  get 'tickets/destroy'
+
   root 'projects#index'
 
-  resources :projects
+  resources :projects do
+    resources :tickets
+  end
 end
--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -25,6 +25,6 @@ RSpec.feature "Users can create new projects" do
     click_button "Create Project"
 
     expect(page).to have_content "Project has not been created."
-    expect(page).to have_content "Name can't be blank"
+    expect(page).to have_content "Namecan't be blank"
   end
 end
