240b1fd04d9e862567fdf14836cd6b956a8ab07b
matk0 <matej@occupationbliss.com>
2016-10-20 07:21:52 +0200

    Remove unnecessary failing spec and reorganize routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-  get 'users/index'
-  end
 
   root 'projects#index'
 
@@ -9,6 +6,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root 'application#index'
+    get 'users/index'
     resources :projects, only: [:new, :create, :destroy]
     resources :users do
       member do
