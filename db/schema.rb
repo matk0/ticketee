1502e8a2c1ae69c2f6ff06905ae2249eeac8b6f1
matk0 <matej@occupationbliss.com>
2016-11-09 15:45:55 +0100

    Admins can now set a default state for tickets.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026064528) do
+ActiveRecord::Schema.define(version: 20161109143255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,8 +58,9 @@ ActiveRecord::Schema.define(version: 20161026064528) do
   add_index "roles", ["user_id"], name: "index_roles_on_user_id", using: :btree
 
   create_table "states", force: :cascade do |t|
-    t.string "name"
-    t.string "color"
+    t.string  "name"
+    t.string  "color"
+    t.boolean "default", default: false
   end
 
   create_table "taggings", force: :cascade do |t|
