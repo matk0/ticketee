6b1290ee90e09fb1154301b3b34214a5f8f3ff08
matk0 <matej@occupationbliss.com>
2016-11-09 13:31:15 +0100

    Prettify states.

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -4,11 +4,24 @@ module TicketsHelper
       content_tag(:p) do
         value = "<strong><i class='fa fa-gear'></i> state changed</strong>"
         if comment.previous_state.present?
-          value += " from #{render comment.previous_state}"
+          value += " from <span class='#{label_style(comment.previous_state.name)} label'>#{render comment.previous_state}</span>"
         end
-        value += " to #{render comment.state}"
+        value += " to <span class='label #{label_style(comment.state.name)}'>#{render comment.state}</span>"
         value.html_safe
       end
+    end
+  end
+
+  def label_style name
+    case name
+    when 'New'
+      "alert"
+    when 'Open'
+      "warning"
+    when 'Closed'
+      "secondary"
+    when 'Awesome'
+      "success"
     end
   end
 end
