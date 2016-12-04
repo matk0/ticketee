5fb7c28fc6ef321c060ca9a3b3656ff9bf809313
matk0 <matej@occupationbliss.com>
2016-12-04 19:51:34 +0100

    Swap fog for carrierwave-aws gem.

--- Gemfile
+++ Gemfile
@@ -16,8 +16,8 @@ gem 'devise', '~> 3.4.1'
 gem 'pundit', '~> 0.3.0'
 gem 'carrierwave', '~> 0.10.0'
 gem 'searcher', git: 'https://github.com/radar/searcher.git'
-gem 'fog', '~> 1.29.0'
 gem 'net-ssh'
+gem 'carrierwave-aws'
 
 group :development, :test do
   gem 'byebug'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,14 +2,17 @@ CarrierWave.configure do |config|
   config.root = Rails.root
 
   if Rails.env.production?
-    config.storage = :fog
-    config.fog_credentials = {
-      provider: "AWS",
-      aws_access_key_id: ENV["S3_KEY"],
-      aws_secret_access_key: ENV["S3_SECRET"],
-      region: ENV["S3_REGION"]
+    config.storage = :aws
+    config.aws_bucket = ENV.fetch('S3_BUCKET')
+    config.aws_acl = 'public-read'
+
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+
+    config.aws_credentials = {
+      access_key_id: ENV.fetch( "S3_KEY" ),
+      secret_access_key: ENV.fetch( "S3_SECRET" ),
+      region: ENV.fetch( "S3_REGION" )
     }
-    config.fog_directory = ENV["S3_BUCKET"]
   else
     config.storage = :file
   end
