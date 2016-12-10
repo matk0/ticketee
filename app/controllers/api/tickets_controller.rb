a1decd170073fb97d0d3160e5fcd0f4e4b3d912e
matk0 <matej@occupationbliss.com>
2016-12-10 09:45:39 +0100

    Fleshed out tickets API with creating tickets and responses for error conditions.

--- app/controllers/api/tickets_controller.rb
+++ app/controllers/api/tickets_controller.rb
@@ -1,13 +1,20 @@
-class API::TicketsController < ApplicationController
-  before_action :authenticate_user
+class API::TicketsController < API::ApplicationController
   before_action :set_project
-
-  attr_reader :current_user
 
   def show
     @ticket = @project.tickets.find(params[:id])
     authorize @ticket, :show?
     render json: @ticket
+  end
+
+  def create
+    @ticket = @project.tickets.build(ticket_params)
+    authorize @ticket, :create?
+    if @ticket.save
+      render json: @ticket, status: 201
+    else
+      render json: { errors: @ticket.errors.full_messages }, status: 422
+    end
   end
 
   private
@@ -16,9 +23,7 @@ class API::TicketsController < ApplicationController
     @project = Project.find(params[:project_id])
   end
 
-  def authenticate_user
-    authenticate_with_http_token do |token|
-      @current_user = User.find_by(api_key: token)
-    end
+  def ticket_params
+    params.require(:ticket).permit(:name, :description)
   end
 end
